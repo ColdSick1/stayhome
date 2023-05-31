eae65c2be7d01b44fd8bbbf75ca45fff2492895d
kirill <kirill.belyanin01@mail.ru>
2023-05-31 09:31:25 +0700

    changes

---
+++ lib/presentation/progress/widgets/progress_tutorial.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProgressTutorial extends StatelessWidget {
+  const ProgressTutorial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text('data'),
+        Text('data'),
+        Text('data'),
+      ],
+    );
+  }
+}
